81c8d3708e886771e5504565754d879757ec2542
MariKa2525 <113701062+MariKa2525@users.noreply.github.com>
2022-11-28 22:49:38 +0200

    add

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryCardsMarkup = createGalleryCardsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryCardsMarkup)
+
+galleryContainer.addEventListener('click', onGalleryContainerClick)
+
+function createGalleryCardsMarkup(galleryItems) {
+return galleryItems.map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+        <a class="gallery__link" href = "${original}">
+            <img
+                class = "gallery__image"
+                src = "${preview}"
+                data-source = "${original}"
+                alt = "${description}"
+            />
+        </a>
+    </div>
+    `;
+    }).join('');
+}
+const gal = document.querySelector('.gallery__link');
+console.log(gal.getAttribute("href"))
+
+
+const instance = basicLightbox.create(`
+<img width="1400" height="900" href = "gal.getAttribute("href")">
+`).show()
+
+// instance.show(() => console.log('lightbox now visible'))
+console.log(instance)
+
+function onGalleryContainerClick(evt) {
+    evt.preventDefault();
+    if(evt.target.nodeName === 'IMG') {
+        // instance.show()
+    }
+    console.log(evt.target.nodeName)
+}
+
